994016a600e480d613efa99b4b5a3293c075a467
samrosenstock1 <113300959+samrosenstock1@users.noreply.github.com>
2024-05-22 07:06:50 -0400

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -22,7 +22,6 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
